3d06f8489504deb92e9129f2f0e01e3b91b013c5
TCMPUser01 <zhx045@health.ucsd.edu>
2022-04-29 15:42:58 -0700

    Initial GitHub

---
+++ DHCSDateUpdateUtility.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace DHCSDateUpdate
+{
+    public class DHCSDateUpdate 
+    {
+        public string DBBassConn { get; set; }
+        public string DBPatsConn { get; set; }
+
+        public string FilePath { get; set; }
+        public string ErrorMessage { get; set; }
+        
+        public void Import()
+        {
+            LogWriter.LogMessageToFile("Get all CDCRs from import file.");
+            //string excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\\DHCSDates\\Q42017Results.xlsx;Extended Properties=\"Excel 12.0;HDR=YES;\"";
+            string CSVFileConnectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"text;HDR=Yes;FMT=Delimited\";", Path.GetDirectoryName(FilePath));
+           
+            try
+            {
+                DataTable dt = new DataTable();
+                using (OleDbConnection con = new OleDbConnection(CSVFileConnectionString))
+                {
+                    con.Open();
+                    var csvQuery = string.Format("select [CDCRNum],[Eligibility Date], [Y/N] from[{0}]", Path.GetFileName(FilePath));
+                    using (OleDbDataAdapter da = new OleDbDataAdapter(csvQuery, con))
+                    {
+                        try {
+                            da.Fill(dt);
+                        }
+                        catch(Exception ex)
+                        {
+                            LogWriter.LogMessageToFile(ex.Message);
+                        }
+                    }
+
+                    dt.TableName = "DHCSTable";
+                }
+
+                LogWriter.LogMessageToFile("Get data Complated");
+
+                if (UpdateDHCSDates(dt))
+                {
+                    LogWriter.LogMessageToFile("All CDCR Updated.");
+                    return;
+                }
+                LogWriter.LogMessageToFile("Failed to update CDCRs.");
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogMessageToFile(ex.Message);
+            }
+        }
+
+        private bool UpdateDHCSDates(DataTable dt)
+        {
+            try
+            {
+                //Update DHCSDates only for Medical
+                using (SqlConnection cnz = new SqlConnection(DBBassConn))
+                {
+                    try
+                    {
+                        cnz.Open();
+                        SqlCommand cmdzs = new SqlCommand();
+                        SqlParameter parameter = new SqlParameter();
+                        cmdzs.Connection = cnz;
+                        cmdzs.Parameters.AddWithValue("@DHCStb", dt);
+                        cmdzs.CommandText = "spUpdateDHCSDates";
+                        cmdzs.CommandType = CommandType.StoredProcedure;
+                        cmdzs.CommandTimeout = 300;
+                        cmdzs.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch (SqlException err)
+                    {
+                        LogWriter.LogMessageToFile(err.Message);                      
+                    }
+                    finally
+                    {
+                        cnz.Close();                       
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogMessageToFile(ex.Message);
+                return false;
+            }
+            
+        }
+
+        ~DHCSDateUpdate() { }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Net.Mail;
+
+namespace DHCSDateUpdate
+{
+    class Program
+    {
+        static bool mailSent = false;
+        static string mailbody = string.Empty;
+        public static string DBBassConn { get; set; }
+        public static string DBPatsConn { get; set; }
+       
+        static void Main(string[] args)
+        {
+             DHCSDateUpdate lib = new DHCSDateUpdate();
+                lib.DBBassConn = ConfigurationManager.ConnectionStrings["BassWeb"].ConnectionString;
+               string path = ConfigurationManager.AppSettings["FilePath"];
+                string filename = Path.Combine(path, "DHCSResults.csv");
+                if (File.Exists(filename))
+                {
+                    lib.FilePath = filename;
+                    mailbody = DateTime.Now.ToString() + " Start read dates..." + Environment.NewLine;
+                    lib.Import();
+                    mailbody = mailbody + DateTime.Now.ToString() + "End dates update..." + Environment.NewLine;
+                   
+                    var newFile = Path.Combine(path, "DHCSResults" + DateTime.Now.ToString("yyyyMMddhhss") + ".csv");
+                    System.IO.File.Move(lib.FilePath, newFile);
+                    string rename = " DHCSResults.csv rename to DHCSResults" + DateTime.Now.ToString("yyyyMMddhhss") + ".csv";
+                    LogWriter.LogMessageToFile(rename);
+                    mailbody = mailbody + DateTime.Now.ToString() + rename + Environment.NewLine;
+                }
+                else
+                {
+                    mailbody = mailbody + DateTime.Now.ToString() + " " + filename + " File Not Found..." + Environment.NewLine;
+                }
+
+                LogWriter.LogMessageToFile("Send email to TCMP ..." + DateTime.Now.ToString());
+                mailbody = mailbody + DateTime.Now.ToString() + " Send email to TCMP ...";
+                SendMail();
+                return;
+            }
+
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                Console.WriteLine("[{0}] Send canceled.", token);
+            }
+            if (e.Error != null)
+            {
+                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+            }
+            else
+            {
+                LogWriter.LogMessageToFile(" Message sent.");
+            }
+            mailSent = true;
+        }
+
+        private static void SendMail()
+        {
+            SmtpClient client = new SmtpClient("smtp.cdcr.ca.gov");
+            MailAddress from = new MailAddress("FDCDBA3_TCMP@cdcr.ca.gov", "TCMP", System.Text.Encoding.UTF8);
+            // Set destinations for the e-mail message.
+            MailAddressCollection toAddresses = new MailAddressCollection();
+            //toAddresses.Add(new MailAddress("Quentin.Miller@cdcr.ca.gov"));
+            toAddresses.Add(new MailAddress("carol.xu@cdcr.ca.gov"));
+
+            // Specify the message content.
+            MailMessage message = new MailMessage();
+            message.From = from;
+            message.To.Add(toAddresses.ToString());
+            message.CC.Add("carol.xu@cdcr.ca.gov");
+            message.Body = "DHCS Update Completed at " + DateTime.Now + ":";
+
+
+            // Include some non-ASCII characters in body and subject.
+            message.Body = mailbody + Environment.NewLine;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Subject = "DHCS Update Completed!";
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            // Set the method that is called back when the send operation ends.
+            client.SendCompleted += new
+            SendCompletedEventHandler(SendCompletedCallback);
+            // The userState can be any object that allows your callback 
+            // method to identify this send operation.
+           string userState = "Update Data";
+           client.SendAsync(message, userState);
+            string answer = Console.ReadLine();
+            // Clean up.
+            message.Dispose();
+            System.Environment.Exit(0);
+            //return;
+        }
+    }
+}
